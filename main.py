36c17f299f8efa6e091996b3ae924a00c0a97f06
lilrainfall <levtsoff2011@yandex.ru>
2024-07-05 12:25:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+print("1st programm")
+print(9**0.5*5)
+
+print("2nd programm")
+print(9.98 < 9.99 and 1000 != 1000.1)
+
+print("3rd programm")
+x=1234
+y=5678
+x1= 1234//10
+y1=5678//10
+x2=x1%100
+y2=y1%100
+print(y2+x2)
+
+print("4th programm")
+x=13.42
+y=42.13
+x1=int(x)
+y1=int(y)
+x2=13.42*100//100
+y2=42.13*100//100
+print(x1, x2, y1, y2)
+print(x2 == x1 or y2 == y1)
